246c49bee403a20f984a2c543244c37c8d0ef35e
UTkarsh <utkarshme96@gmail.com>
2016-12-21 23:28:30 +0530

    Add winston for logging

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 var fs = require("fs");
 
+// Using winston for logging
+var winston = require("winston");
+var log = new (winston.Logger)({
+    transports: [
+        new (winston.transports.File)({
+            name: "info-file",
+            filename: "./logs/server_info.log",
+            level: "info"
+        }),
+        new (winston.transports.File)({
+            name: "error-file",
+            filename: "./logs/server_error.log",
+            level: "error"
+        })
+    ]
+});
+
+
 // Read config file
 var config = JSON.parse(fs.readFileSync("./config.json"));
 
@@ -9,3 +27,4 @@ var app = express();
 
 // Listen to app
 app.listen(config.port, config.host);
+log.info("Listening on", config.host + ":" + config.port);
