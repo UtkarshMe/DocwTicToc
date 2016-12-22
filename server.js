34bdb0c76673dcc1d591c6d4c209d614dd0e4efd
UTkarsh <utkarshme96@gmail.com>
2016-12-22 18:30:56 +0530

    Modify config file to store logger configs

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 var fs = require("fs");
 
+// Read config file
+var config = JSON.parse(fs.readFileSync("./config.json"));
+
 // Using winston for logging
 var winston = require("winston");
 var log = new (winston.Logger)({
     transports: [
+        new (winston.transports.Console)(),
         new (winston.transports.File)({
-            name: "info-file",
-            filename: "./logs/server_info.log",
-            level: "info"
-        }),
-        new (winston.transports.File)({
-            name: "error-file",
-            filename: "./logs/server_error.log",
-            level: "error"
+            filename: config.log.file,
+            level: config.log.level.file
         })
     ]
 });
-
-
-// Read config file
-var config = JSON.parse(fs.readFileSync("./config.json"));
+// Set logging level for console
+log.level = config.log.level.console;
 
 // Create app
 var express = require("express");
 var app = express();
+
+// Use public directory for static files
+app.use(express.static(__dirname + "/public"));
+
 
 // Listen to app
 app.listen(config.port, config.host);
