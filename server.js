4333b8d4e596628622ce06dc241a1f20f80de8d5
UTkarsh <utkarshme96@gmail.com>
2016-12-24 01:48:23 +0530

    Switch to morgan for logging

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 // Require all packages
 var express = require('express');
 var fs = require('fs');
-var winston = require('winston');
-var expressWinston = require('express-winston');
 var passport = require('passport');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var morgan = require('morgan');
 
 // Read config file
 var config = JSON.parse(fs.readFileSync('./config/config.json'));
@@ -19,28 +17,8 @@ var app = express();
 app.set('view engine', 'ejs');
 
 
-// Using winston for logging
-var log = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({
-            colorize: true,
-            level: config.log.level.console
-        }),
-        new (winston.transports.File)({
-            filename: config.log.file,
-            level: config.log.level.file
-        })
-    ]
-});
-// Use winston for logs by express
-app.use(expressWinston.logger({
-    transports: [
-        new winston.transports.Console({
-            colorize: true,
-            level: "verbose"
-        })
-    ]
-}));
+//Use morgan for logging
+app.use(morgan('tiny'));
 
 
 // Connect database
@@ -65,4 +43,4 @@ app.use(express.static(__dirname + '/public'));
 
 // Listen to app
 app.listen(config.port, config.host);
-log.info('Listening on', config.host + ':' + config.port);
+console.log('Listening on', config.host + ':' + config.port);
