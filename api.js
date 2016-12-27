c07b8725a2b90f15877613c01c3f9a99e5efd35c
UTkarsh <utkarshme96@gmail.com>
2016-12-27 19:34:10 +0530

    Get leaderboard working
     - Make an api call for leaderboard
     - Use ajax to get data from api
     - Update leaderboard

--- api.js
+++ api.js
@@ -15,11 +15,35 @@ module.exports = function(app, passport){
             }
         });
     });
+
+    app.get('/api/leaderboard/', isLoggedIn, function(req, res){
+        var Teams = require('./models/team.js');
+        var query = Teams.find({});
+
+        query.select('local.username local.game.score');
+
+        query.exec(function(err, teams){
+
+            if (err) {
+                throw err;
+            }else{
+                var content = [];
+                teams.forEach(function (team) {
+                    if (team.local.username != 'admin')
+                        content.push(team.local);
+                });
+                content.sort(function(a, b){
+                    return a.game.score - b.game.score;
+                });
+                res.send(JSON.stringify(content));
+            }
+        });
+    });
 }
 
 
-function isLoggedIn(req, res) {
-    if (req.authenticated()) {
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
     res.send();
---
+++ public/js/leaderboard.js
@@ -0,0 +1,26 @@
+function loadLeaderboard() {
+    var leaderXhttp = new XMLHttpRequest();
+    leaderXhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var teams = JSON.parse(this.responseText);
+
+            var out ='<tbody>';
+            var count = 1;
+            teams.forEach(function(team){
+                out += '<tr>';
+                out += '<td>#' + count++ + '</td>';
+                out += '<td>' + team.username + '</td>';
+                out += '<td>' + team.game.score + '</td>';
+                out += '</tr>';
+            });
+            out += '</tbody>';
+
+            document.getElementById('leaderboard').innerHTML = out;
+        }
+    }
+        leaderXhttp.open("GET", "/api/leaderboard", true);
+        leaderXhttp.send();
+};
+
+loadLeaderboard();
+setTimeout(loadLeaderboard, 3000);
