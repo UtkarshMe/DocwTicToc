3b643eb4a1c224318bc793b5c67713742969a1d8
UTkarsh <utkarshme96@gmail.com>
2017-01-18 10:53:24 +0530

    Improve date in news

--- public/js/news.js
+++ public/js/news.js
@@ -16,16 +16,22 @@ function loadNews() {
                 }else{
                     icon = 'question-sign';
                 }
+                var month = ["January", "February", "March", "April",
+                    "May", "June", "July", "August", "September", "October",
+                    "November", "December"];
 
 
                 out += '<div class="col-md-12">';
                 out += '<div class="media">';
                 out += '<div class="media-left">';
-                out += '<span class="media-object glyphicon glyphicon-' + icon + '" aria-hidden="true"></span>';
+                out += '<span class="media-object glyphicon glyphicon-'
+                    + icon + '" aria-hidden="true"></span>';
                 out += '</div><div class="media-body">';
-                out += '<h4 class="media-heading">' + newsItem.title + '<br />';
-                out += '<small>' + date.getHours() + ':' + date.getMinutes() + ', ';
-                out += date.getDate() + '/' + date.getMonth()+1 + '</small></h4>';
+                out += '<h4 class="media-heading">' + newsItem.title +'<br />';
+                out += '<small>';
+                out += date.getDate() + ' ' + month[date.getMonth()];
+                out += ' on ' + date.getHours() + ':' + date.getMinutes();
+                out += '</small></h4>';
                 out += '<p>' + newsItem.content + '</p>';
                 out += '<br /> <br />';
                 out += '</div></div></div>';
